43524b36dd9bf1225e2c9e88386e276d53b18e28
thwogjs987 <thwogjs987>
2021-10-12 18:22:11 +0900

    Update untracked files

---
+++ app/src/main/java/com/gyeongsotone/danplay/Mainpage.kt
@@ -0,0 +1,19 @@
+package com.gyeongsotone.danplay
+
+import android.view.ViewGroup
+import android.view.LayoutInflater
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class Mainpage : Fragment() {
+    var viewGroup: ViewGroup? = null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewGroup = inflater.inflate(R.layout.fragment_mainpage, container, false) as ViewGroup
+        return viewGroup
+    }
+}
