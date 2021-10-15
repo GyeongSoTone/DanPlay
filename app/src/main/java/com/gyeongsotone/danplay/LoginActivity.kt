780dc01aa6d9b5efc263fcdd4351288bb07a4c5f
Yunkeun <yunx2@naver.com>
2021-10-15 21:43:12 +0900

    Change file name

--- app/src/main/java/com/gyeongsotone/danplay/LoginActivity.kt
+++ app/src/main/java/com/gyeongsotone/danplay/LoginActivity.kt
@@ -1,14 +1,12 @@
 package com.gyeongsotone.danplay
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+        setContentView(R.layout.activity_login)
     }
 }
--- app/src/main/java/com/gyeongsotone/danplay/SignupActivity.kt
+++ app/src/main/java/com/gyeongsotone/danplay/SignupActivity.kt
@@ -1,14 +1,12 @@
 package com.gyeongsotone.danplay
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 
 class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sign_up)
+        setContentView(R.layout.activity_signup)
     }
 }
