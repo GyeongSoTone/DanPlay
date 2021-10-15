7cac59e913194fd712c0a35419189edae7e15b46
TaegyuCho <wh2446@naer.com>
2021-10-16 00:03:59 +0900

    add viewbinding in login and signup

--- app/src/main/java/com/gyeongsotone/danplay/LoginActivity.kt
+++ app/src/main/java/com/gyeongsotone/danplay/LoginActivity.kt
@@ -1,12 +1,29 @@
 package com.gyeongsotone.danplay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gyeongsotone.danplay.databinding.ActivityLoginBinding
+import com.gyeongsotone.danplay.databinding.ActivitySignupBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private var mBinding: ActivityLoginBinding? = null
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        mBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUp.setOnClickListener{
+            startActivity(Intent(this, SignupActivity::class.java))
+            finish()
+        }
+
+        binding.login.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/gyeongsotone/danplay/SignupActivity.kt
+++ app/src/main/java/com/gyeongsotone/danplay/SignupActivity.kt
@@ -1,12 +1,27 @@
 package com.gyeongsotone.danplay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.gyeongsotone.danplay.databinding.ActivityLoginBinding
+import com.gyeongsotone.danplay.databinding.ActivitySignupBinding
+
 
 class SignupActivity : AppCompatActivity() {
 
+    private var mBinding: ActivitySignupBinding? = null
+    private val binding get() = mBinding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+
+        mBinding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSignUp.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
