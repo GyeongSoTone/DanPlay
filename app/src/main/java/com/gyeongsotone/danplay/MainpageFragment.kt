705fb220bb5c031d673941f58463b2f4c2a917f2
thwogjs987 <thwogjs987>
2021-10-29 18:34:07 +0900

    메인 페이지 본인 이름으로 변경 회원탈퇴, 로그아웃 구현

--- app/src/main/java/com/gyeongsotone/danplay/MainpageFragment.kt
+++ app/src/main/java/com/gyeongsotone/danplay/MainpageFragment.kt
@@ -6,16 +6,37 @@ import android.view.LayoutInflater
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainpageFragment : Fragment() {
     var viewGroup: ViewGroup? = null
+    private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         viewGroup = inflater.inflate(R.layout.fragment_mainpage, container, false) as ViewGroup
+        database = Firebase.database.reference
+        auth = FirebaseAuth.getInstance()
+        var current_user = auth.currentUser!!
+        var name = viewGroup!!.findViewById<View>(R.id.text_name) as TextView
+
+        database.child("user").child(current_user.uid).get().addOnSuccessListener {
+            //Toast.makeText(this, "${it.child("name").value}님 환영합니다!", Toast.LENGTH_LONG).show()
+            name.setText("${it.child("name").value}")
+        }.addOnFailureListener{
+            //Toast.makeText(this, "실패~", Toast.LENGTH_LONG).show()
+        }
+
         var btn_sports1 = viewGroup!!.findViewById<View>(R.id.button_sports1) as ImageButton
         btn_sports1.setOnClickListener {
             val intent = Intent(requireActivity().applicationContext, SignupActivity::class.java)
--- app/src/main/java/com/gyeongsotone/danplay/MyinfoFragment.kt
+++ app/src/main/java/com/gyeongsotone/danplay/MyinfoFragment.kt
@@ -1,19 +1,62 @@
 package com.gyeongsotone.danplay
 
+import android.content.Intent
 import android.view.ViewGroup
 import android.view.LayoutInflater
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MyinfoFragment : Fragment() {
     var viewGroup: ViewGroup? = null
+    private lateinit var database: DatabaseReference
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         viewGroup = inflater.inflate(R.layout.fragment_myinfo, container, false) as ViewGroup
+        database = Firebase.database.reference
+        auth = FirebaseAuth.getInstance()
+        var current_user = auth.currentUser!!
+        var btn_signout = viewGroup!!.findViewById<View>(R.id.signout) as Button
+        var btn_logout = viewGroup!!.findViewById<View>(R.id.logout) as Button
+
+        btn_signout.setOnClickListener {
+            database.child("user").child(current_user.uid).removeValue()
+                .addOnSuccessListener {
+                    // Write was successful!
+                    // ...
+                }
+                .addOnFailureListener {
+                    // Write failed
+                    // ...
+                }
+            current_user.delete()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        //Log.d(TAG, "User account deleted.")
+                        Toast.makeText(getActivity(), "회원탈퇴 완료.", Toast.LENGTH_LONG).show()
+                        val intent = Intent(requireActivity().applicationContext, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+        }
+        btn_logout.setOnClickListener {
+            Toast.makeText(getActivity(), "로그아웃 완료.", Toast.LENGTH_LONG).show()
+            val intent = Intent(requireActivity().applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
         return viewGroup
     }
 }
