12c7943056d54ccc332f152746ba10171a2470b1
HongdeokKIm <qchdkim@naver.com>
2021-10-31 16:04:58 +0900

    날짜 입력 후 시간 입력 받도록 함

--- app/src/main/java/com/gyeongsotone/danplay/ApplyFragment.kt
+++ app/src/main/java/com/gyeongsotone/danplay/ApplyFragment.kt
@@ -25,9 +25,9 @@ import java.util.*
 class ApplyFragment : Fragment() {
     var viewGroup: ViewGroup? = null
     private lateinit var database: DatabaseReference
-    var g_sport: String? = null
-    var g_people: Int? = null
-    var g_place: String? = null
+    private var g_sport: String? = null
+    private var g_people: Int? = null
+    private var g_place: String? = null
 
 
     override fun onCreateView(
@@ -57,7 +57,7 @@ class ApplyFragment : Fragment() {
                 .setPositiveButton("확인") { dialog, which ->
                     g_sport = selectedItem.toString()
                     //Toast.makeText(getActivity(),g_sport, Toast.LENGTH_SHORT).show()
-                    btn_event.setText(g_sport)
+                    btn_event.text = g_sport
                 }
                 .show()
         }
@@ -74,7 +74,7 @@ class ApplyFragment : Fragment() {
                     selectedItem = people[which]
                 }
                 .setPositiveButton("확인") { dialog, which ->
-                    btn_people.setText(selectedItem)
+                    btn_people.text = selectedItem
                     g_people = selectedItem!!.toInt()
                     //Toast.makeText(getActivity(),g_people.toString(), Toast.LENGTH_SHORT).show()
                 }
@@ -95,26 +95,7 @@ class ApplyFragment : Fragment() {
                 .setPositiveButton("확인") { dialog, which ->
                     g_place = selectedItem.toString()
                     //Toast.makeText(getActivity(),g_sport, Toast.LENGTH_SHORT).show()
-                    btn_place.setText(g_place)
-                }
-                .show()
-        }
-
-        btn_place.setOnClickListener {
-            val place = arrayOf<String>("노천마당", "대운동장", "인문관소극장", "족구장", "테니스장A",
-                "테니스장C", "평화의광장 농구장A", "평화의광장 농구장B", "평화의광장 농구장C", "폭포공원",
-                "풋살경기장", "혜당관(학생극장 2층로비)", "혜당관(학생회관)212(학생극장)")
-            var selectedItem: String? = null
-
-            val builder = AlertDialog.Builder(getActivity())
-                .setTitle("장소를 선택하세요")
-                .setSingleChoiceItems(place, -1) { dialog, which ->
-                    selectedItem = place[which]
-                }
-                .setPositiveButton("확인") { dialog, which ->
-                    g_place = selectedItem.toString()
-                    //Toast.makeText(getActivity(),g_sport, Toast.LENGTH_SHORT).show()
-                    btn_place.setText(g_place)
+                    btn_place.text = g_place
                 }
                 .show()
         }
@@ -131,16 +112,23 @@ class ApplyFragment : Fragment() {
                 year = i
                 month = i2
                 day = i3
-                btn_time.setText("${year}년 ${month}월 ${day}일")
+
+                var listener2 = TimePickerDialog.OnTimeSetListener { timePicker, i, i2 ->
+                    hour = i
+                    minute = i2
+                    btn_time.text = "${year}년 ${month}월 ${day}일 ${i}시 ${i2}분"
+                }
+                var picker2 = TimePickerDialog(getActivity(), listener2, hour, minute, false ) // true하면 24시간 제
+                picker2.show()
             }
+
+
             var picker1 = getActivity()?.let { it1 -> DatePickerDialog(it1, listener1, year, month, day) }
             picker1!!.show()
 
-            var listener2 = TimePickerDialog.OnTimeSetListener { timePicker, i, i2 ->
-                btn_time.setText("${i}시 ${i2}분")
-            }
-            var picker2 = TimePickerDialog(getActivity(), listener2, hour, minute, false ) // true하면 24시간 제
-            picker2.show()
+
+
+
         }
         return viewGroup
     }
